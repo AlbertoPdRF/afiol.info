7430dfa2949c3e7f046deec7becc68260fe515cc
AlbertoPdRF <albertopdrf.wca@gmail.com>
2019-01-30 15:36:48 +0100

    Add Start Bootstrap Creative theme.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def page_title(page_title = "")
+    base_title = "Alberto Pérez de Rada Fiol"
+    if page_title.blank?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
+  def fa_icon(id)
+    content_tag :i, "", class: "fa fa-#{id}"
+  end
 end
---
+++ vendor/assets/javascripts/creative/creative_mainfest.js
@@ -0,0 +1,10 @@
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap
+//= require creative/cbpAnimatedHeader
+//= require creative/classie
+//= require jquery.easing
+//= require creative/jquery.fittext
+//= require creative/wow.min
+//= require creative/creative
