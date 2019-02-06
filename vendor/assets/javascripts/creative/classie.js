68ccd4f8e9f48ca54ec1fa4a7730a4095b2116bd
AlbertoPdRF <albertopdrf.wca@gmail.com>
2019-02-06 22:25:40 +0100

    Fix resposiveness.

--- vendor/assets/javascripts/creative/classie.js
+++ vendor/assets/javascripts/creative/classie.js
@@ -1,7 +1,7 @@
 /*!
  * classie - class helper functions
  * from bonzo https://github.com/ded/bonzo
- * 
+ *
  * classie.has( elem, 'my-class' ) -> true/false
  * classie.add( elem, 'my-new-class' )
  * classie.remove( elem, 'my-unwanted-class' )
@@ -13,6 +13,7 @@
 
 
 
+$(document).ready(function(){
 
 ( function( window ) {
 
@@ -82,3 +83,4 @@ if ( typeof define === 'function' && define.amd ) {
 
 })( window );
 
+})
--- vendor/assets/javascripts/creative/creative.js
+++ vendor/assets/javascripts/creative/creative.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
       })
 
       // Closes the Responsive Menu on Menu Item Click
-      $('.navbar-collapse ul li a').click(function() {
+      $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
           $('.navbar-toggle:visible').click();
       });
 
@@ -50,4 +50,3 @@ $(document).ready(function(){
   })(jQuery); // End of use strict
 
 })
-
