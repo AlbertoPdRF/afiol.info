09db7eb55e89130d6872526f948b8bdf2c62d7ec
AlbertoPdRF <albertopdrf.wca@gmail.com>
2019-02-07 19:48:51 +0100

    Show default locale in URLs too, and re-order `routes.rb`.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  root_path = 'pages#homepage'
-
   scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
+    root to: 'pages#homepage'
+
     get '/about' => 'pages#about'
     get '/research' => 'pages#research'
     get '/speedcubing' => 'pages#speedcubing'
     get '/contact' => 'pages#contact'
   end
-
-  root to: root_path
-  get '/:locale', to: root_path
 end
